f4e664a09af98c5aefa65f25534d75b8a68e27a9
Thibault Clérice <leponteineptique@gmail.com>
2019-05-28 19:07:06 +0200

    Removed zero width no break space

--- test_data/bugged.py
+++ test_data/bugged.py
@@ -5,10 +5,10 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-label = LabelEncoder(masked=True)
+label = LabelEncoder(masked=True, lower=False, remove_diacriticals=True)
 label.build("bugged.txt", debug=True)
 
 dataset = label.get_dataset("bugged.txt")
-batch_generator = dataset.get_epoch(batch_size=32)()
+batch_generator = dataset.get_epoch(batch_size=64)()
 batch = next(batch_generator)
 print(batch)
