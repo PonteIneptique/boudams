14b5ec05a31a8d5553217837529245e561443439
Thibault Clérice <leponteineptique@gmail.com>
2019-06-08 11:24:15 +0200

    Article update

--- article/code_excerpt.py
+++
@@ -1,9 +0,0 @@
-import mufidecode
-import unidecode
-"sot la gnͣt abstinence dess eintes uirges ele  ꝑla"
-mufidecode.mufidecode(" sot la gnͣt abstinence dess eintes uirges ele  ꝑla")
-# ' sot la gnat abstinence dess eintes uirges ele  pla'
-mufidecode.mufidecode(" sot la gnͣt abstinence dess eintes uirges ele  ꝑla", join=False
-# (' ',  's',  'o',  't',  ' ',  'l',  'a',  ' ',  'g',  'n',  'a',  't',  ' ',  'a',  'b',  's',  't',  'i',  'n',  'e',  'n',  'c',  'e',  ' ',  'd',  'e',  's',  's',  ' ',  'e',  'i',  'n',  't',  'e',  's',  ' ',  'u',  'i',  'r',  'g',  'e',  's',  ' ',  'e',  'l',  'e',  ' ',  ' ',  'p',  'l',  'a')
-unidecode.unidecode(" sot la gnͣt abstinence dess eintes uirges ele  ꝑla")
-# ' sot la gnat abstinence dess eintes uirges ele  la'
